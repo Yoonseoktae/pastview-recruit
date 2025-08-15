26033e8f7df2aec50873f4de49d4cda93257f4d3
ebridge-tomas <153583901+ebridge-tomas@users.noreply.github.com>
2025-08-15 20:40:15 +0900

    게시글/댓글 마이그레이션

---
+++ database/migrations/2025_08_15_112416_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            // 컬럼
+            $table->id();
+            $table->string('title', 255)->comment('제목');
+            $table->text('content')->comment('내용');
+            $table->string('author', 50)->comment('작성자');
+            $table->tinyInteger('is_delete')->comment('삭제여부');
+            $table->timestamps();
+
+            // 인덱스
+            $table->index(['author']);
+            $table->index(['is_delete', 'created_at'], 'base_search_idx');
+            $table->fullText(['title', 'content'], 'title_content_idx');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2025_08_15_112423_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            // 컬럼
+            $table->id();
+            $table->unsignedBigInteger('post_id')->comment('게시글 ID');
+            $table->text('content')->comment('댓글 내용');
+            $table->string('author', 50)->comment('댓글 작성자');
+            $table->tinyInteger('is_delete')->comment('삭제 여부');
+            $table->timestamps();
+            
+            // 인덱스
+            $table->index(['post_id', 'created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
