21caf56e426b30b15c58bc08897c0110821a21fe
ebridge-tomas <153583901+ebridge-tomas@users.noreply.github.com>
2025-08-15 20:51:56 +0900

    게시글/댓글 모델 생성

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'author',
+        'is_delete'
+    ];
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
